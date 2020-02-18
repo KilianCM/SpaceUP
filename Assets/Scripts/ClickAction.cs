c9446dc3db002abd2081baffa499a235bcdaa04b
Paolo DA-COSTA <dacostap@iut-acy.local>
2020-02-18 16:26:51 +0100

    creation of the module combustion

---
+++ Assets/Scripts/ClickAction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAction : MonoBehaviour
+{
+    [SerializeField]
+    private Material materialTrue;
+    private Material materialDefault;
+
+    
+    public bool isActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialDefault = gameObject.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnMouseDown()
+    {
+        isActive= !isActive;
+
+        Renderer r = gameObject.GetComponent<Renderer>();
+
+        
+
+        if (isActive == false)
+        {
+            r.material = materialDefault;
+        }
+        else
+        {
+            r.material = materialTrue;
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/WorkingEngine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkingEngine : MonoBehaviour
+{
+    public ClickAction oxygen;
+    public ClickAction rp1;
+
+    public ParticleSystem particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(oxygen.isActive);
+        if (oxygen.isActive && rp1.isActive)
+        {
+            particle.Play();
+        }
+        else
+            particle.Stop();
+    }
+}
