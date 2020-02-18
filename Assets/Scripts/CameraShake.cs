b261a8e07f232cf83e5c18f1bae81beb39b4c8ee
kchamiot <chamiot.k@gmail.com>
2020-02-18 13:53:42 +0100

    Initial commit

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//@https://gist.github.com/ftvs/5822103
+public class CameraShake : MonoBehaviour
+{
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	public Transform camTransform;
+
+	// How long the object should shake for.
+	public float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.7f;
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+	bool enable=false;
+	void Awake()
+	{
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	void Update()
+	{
+		if (enable)
+		{
+			if (shakeDuration > 0)
+			{
+				camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+				shakeDuration -= Time.deltaTime * decreaseFactor;
+			}
+			else
+			{
+				shakeDuration = 0f;
+				camTransform.localPosition = originalPos;
+			}
+		}
+	}
+
+	public void ShakeEnable(bool isEnable = true)
+	{
+		enable = isEnable;
+	}
+}
