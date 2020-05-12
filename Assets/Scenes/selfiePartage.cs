119b996d280669bcecfdc06496c26117ae9ce8bc
msteralexis <alexis.escudero195@gmail.com>
2020-05-12 11:39:55 +0200

    partage selfi

--- Assets/Scenes/selfiePartage.cs
+++ Assets/Scenes/selfiePartage.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 using UnityEngine.UI;
 
+using VoxelBusters;
+using VoxelBusters.NativePlugins;
+
 public class selfiePartage : MonoBehaviour
 {
 
@@ -13,23 +16,17 @@ public class selfiePartage : MonoBehaviour
 	WebCamTexture cam;
 	public RawImage display;
 
-
 	public void TakePicture(){
-
 		GameObject button = GameObject.Find("Pictur");
 		button.SetActive(false);
-
-		ScreenCapture.CaptureScreenshot("capture.png");
-
-		
-
+		StartCoroutine (CaptureScreenShoot());
 	}
 
 
 	
 
-    void Start()
-    {
+	void Start()
+    	{
 
 
 		// inverse camera pour obtenir la camera de face (je laisse sous cette forme pour l'instant c'es fais expres		
@@ -46,13 +43,43 @@ public class selfiePartage : MonoBehaviour
 		display.texture = cam;
 		cam.Play();
  
+    	}
+
+	
+    public void RateMyApp()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            NPBinding.Utility.OpenStoreLink ("com.RoixoGames.TwinSuns");
+        }
+    }
+ 
+    IEnumerator CaptureScreenShoot ()
+    {
+        yield return new WaitForEndOfFrame ();
+        Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture ();
+        ShareSheet (texture);
+        Object.Destroy (texture);
+	GameObject button = GameObject.Find("Pictur");
+	button.SetActive(true);
+           
+    }
+ 
+    private void ShareSheet (Texture2D texture)
+    {
+        ShareSheet _shareSheet = new ShareSheet ();
+        _shareSheet.Text = "Space Up C'est trosp de la bal !! ";
+        _shareSheet.AttachImage (texture);
+        _shareSheet.URL = "Sur le site de la Turbine";
+        NPBinding.Sharing.ShowView (_shareSheet, FinishSharing);
+    }
+ 
+    private void FinishSharing (eShareResult _result)
+    {
+        Debug.Log (_result);
     }
 
- 
-    void Update()
-    {
-	
-    }
+
 }
 
 
