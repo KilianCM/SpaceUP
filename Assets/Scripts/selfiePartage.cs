8305be21b41d4572c8983fcc69fed17a0ee518dc
kchamiot <chamiot.k@gmail.com>
2020-06-23 10:49:07 +0200

    Rework selfie

--- Assets/Scripts/selfiePartage.cs
+++ Assets/Scripts/selfiePartage.cs
@@ -1,61 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
 using UnityEngine.UI;
-
 using VoxelBusters;
 using VoxelBusters.NativePlugins;
 
-public class selfiePartage : MonoBehaviour
+public class SelfiePartage : MonoBehaviour
 {
 
-	// Oirentation es stocker dans une variable au ca ou on souhaterai développer l'options d'inverser l'image
+	// Orientation stockée dans une variable au cas pour pouvoir inverser l'image
    	int orientation = 1;
 	// Objet camera de notre téléphone
 	WebCamTexture cam; 
-
 	GameObject button;
-
 	// affichage du flux video de la camera sur une l'objet RawImage
 	public RawImage display;
 
-
 	void Start() {
-		// Vérifie si il y à bien une camera de face.	
+		// Vérifie si il y a bien une camera frontale	
 		if(WebCamTexture.devices.Length > 1) {
 
-
 			WebCamDevice cameraSelection =  WebCamTexture.devices[orientation];
-			cam = new WebCamTexture(cameraSelection.name, Screen.width, Screen.height);
-			
+			cam = new WebCamTexture(cameraSelection.name, Screen.width, Screen.height);			
 
-			// si on n'as bien une camera mais que le script à planter
+			// si on a bien une camera mais que le script a planté
 			if(cam == null){
-				Debug.Log(" Problème lors du chargement de la caméra. ");
+				Debug.Log("Problème lors du chargement de la caméra. ");
 				return;
 			}
 
-
-
-
-		}else{
-			Debug.Log("Pas de Caméra détecter");
+		} else {
+			Debug.Log("Pas de caméra détectée");
 			return;
 		}
-		cam.Play();
-		display.texture = cam;
 
-		
- 
-    	}
+        cam.Play();
+		display.texture = cam;
+    }
 
 
 	/* 
-	Fonction déclenchée lors du click sur le Button Pictur de l'utilisateur.
- 	Nous prenons un screen de l'écran avec le logo spaceup et le flux vidéo projeter sur une Raw image.
- 	Nous cacherons donc le boutton déclencheur de l'appareil photo avant le screenn écran.
+	    Fonction déclenchée lors du click sur le Button Pictur de l'utilisateur.
+ 	    Nous prenons un screen de l'écran avec le logo spaceup et le flux vidéo projeté sur une Raw image.
+ 	    Nous cacherons donc le bouton déclencheur de l'appareil photo avant le screen écran.
 	*/
 	public void TakePicture(){
 		button = GameObject.Find("Pictur");
@@ -63,13 +50,6 @@ public class selfiePartage : MonoBehaviour
 		StartCoroutine (CaptureScreenShoot());
 	}
 
-	
-	public void RateMyApp(){
-		if (Application.platform == RuntimePlatform.Android){
-			NPBinding.Utility.OpenStoreLink ("com.RoixoGames.TwinSuns");
-		}
-	}
- 
 	IEnumerator CaptureScreenShoot (){
 		yield return new WaitForEndOfFrame ();
 		Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture ();
@@ -78,15 +58,14 @@ public class selfiePartage : MonoBehaviour
 		button.SetActive(true);	   
 	}
 
- 
 
 	// Partage de la Photo
 	private void ShareSheet (Texture2D texture) {
 		ShareSheet _shareSheet = new ShareSheet ();
-		// text ajouter par defaults (senser être vendeur du site)
-		_shareSheet.Text = "SpaceUp c'est trop cool !";
+		// text ajouté par default
+		_shareSheet.Text = "SpaceUP c'est génial !";
 		_shareSheet.AttachImage (texture);
-		_shareSheet.URL = "Venez sur le site de la Turbine (Cran-gevrier)";
+		_shareSheet.URL = "Visitez le site de la Turbine (Cran-Gevrier), on apprend en s'amusant";
 		NPBinding.Sharing.ShowView (_shareSheet, FinishSharing);
 	}
  
@@ -94,15 +73,5 @@ public class selfiePartage : MonoBehaviour
 		Debug.Log (_result);
 	}
 
-
-}
-
-
-
-
-
-
-
-
-   	
+}   	
 		
