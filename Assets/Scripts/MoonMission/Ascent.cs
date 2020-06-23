7fddb9fd1370c77bc95b874c025ef9b436d302a1
YannickVeron <yveron74@gmail.com>
2020-06-23 15:08:53 +0200

    [Update] Move file in correct folder and add fuel consumption

---
+++ Assets/Scripts/MoonMission/Ascent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ascent : MonoBehaviour
+{
+    public ParticleSystem plume;
+    public float finalAltitude;
+    public GameObject endPanel; 
+
+    private float ascMaxThrust = 15000;
+    private float ascDryMass = 2445;
+    private float ascPropMass = 2376;
+    private Rigidbody rb;
+    private bool isTakingOff = false;
+    private float startingAlt;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(isTakingOff)
+        {
+            if (transform.position.y >= finalAltitude && !endPanel.activeSelf)
+            {
+                endPanel.SetActive(true);
+            }
+            rb.AddForce(new Vector3(0, ascMaxThrust, 0), ForceMode.Force);
+        }
+    }
+
+    public void AscentStart()
+    {
+        startingAlt = transform.position.y;
+        rb=gameObject.AddComponent<Rigidbody>();
+        rb.mass = ascDryMass + ascPropMass;
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
+        isTakingOff = true;
+        plume.Play();
+    }
+}
--- Assets/Scripts/MoonMission/Landing.cs
+++ Assets/Scripts/MoonMission/Landing.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Landing : MonoBehaviour
 {
     public float lHeight = -0.186f;
     public float initSpeed = 10;
+    public ParticleSystem plume;
+    public Button takeOffBtn;
 
     private float descMaxThrust = 45000;
     private float ascMaxThrust = 15000;
@@ -14,9 +17,11 @@ public class Landing : MonoBehaviour
     private float ascDryMass = 2445;
     private float ascPropMass = 2376;
     private bool landed = false;
+    private bool isTakingOff = false;
     private Vector3 gravity = new Vector3(0, -1.62f, 0);
     private Rigidbody rb;
     private float startAlt;
+    private ParticleSystem.MainModule plumeMain;
 
     void Awake()
     {
@@ -29,30 +34,40 @@ public class Landing : MonoBehaviour
         startAlt = transform.position.y - lHeight;
         rb = this.GetComponent<Rigidbody>();
         rb.velocity = new Vector3(0, initSpeed, 0);
+        plumeMain = plume.main;
+        plume.Play();
     }
 
     // Update is called once per frame
     void FixedUpdate()//use of FixedUpdate to sync with gravity computing
     {
-        rb.mass = descDryMass + descPropMass + ascDryMass + ascPropMass;
         if (!landed)
         {
-            float descentForce = rb.mass * rb.velocity.y;
             float currentAlt = transform.position.y - lHeight;
+            float remainingDescPropMass = descPropMass * (currentAlt / startAlt);
+            rb.mass = descDryMass + remainingDescPropMass + ascDryMass + ascPropMass;
+            float descentForce = rb.mass * rb.velocity.y;
             float distributedCounterForce = -(descentForce * (1-(currentAlt / startAlt)));
-            Debug.Log(distributedCounterForce+" / "+descentForce);
             float counterThrottle = 1/(descMaxThrust / distributedCounterForce);
-            //Debug.Log(counterThrottle);
             float twr = descMaxThrust / (rb.mass * (Physics.gravity.y*-1));
             float hover_throttle = 1 / twr;
             float throttle = hover_throttle *(1+ counterThrottle);
-            Debug.Log(throttle);
             rb.AddForce(new Vector3(0,throttle* descMaxThrust, 0),ForceMode.Force);
+            plumeMain.startSpeed = 50 * throttle;
         }
+    }
+
+    IEnumerator disablePhysicsCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        takeOffBtn.gameObject.SetActive(true);
+        Destroy(rb);
     }
 
     void OnCollisionEnter(Collision collision)
     {
         landed = true;
+        plume.Stop();
+        StartCoroutine(disablePhysicsCoroutine());
     }
 }
